30435a459e591a8aa3b87b907bd96bd78126defb
Anton Sizikov <asizikov@github.com>
2023-12-01 10:02:00 +0300

    🎄 add day 01

---
+++ aoc-2023/Program.cs
@@ -0,0 +1,17 @@
+﻿using aoc_2023;
+using aoc_2023.Solvers;
+
+var dayNumberOverride = "";
+var dayNumber = dayNumberOverride == "" ? DateTime.Now.Day.ToString("00") : dayNumberOverride;
+
+var solver = SolversProvider.Get(dayNumber);
+var dataFileName = solver.FileName;
+
+Console.WriteLine($"Processing {dataFileName} for day {dayNumber}");
+Console.WriteLine("----------------");
+
+var input = DataProvider.ReadLines(dataFileName);
+solver.Solve(input);
+
+Console.WriteLine("--------------");
+

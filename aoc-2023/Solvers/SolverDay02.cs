2a86d4c3bde72ebd1c337abdc322fb8394b46255
Anton Sizikov <anton.sizikov@pm.me>
2023-12-16 23:07:22 +0100

    📅 bacfill day 02, part 2

--- aoc-2023/Solvers/SolverDay02.cs
+++ aoc-2023/Solvers/SolverDay02.cs
@@ -5,39 +5,21 @@ public class SolverDay02 : Solver
     [PuzzleInput("02-02")]
     public override void Solve(string[] input)
     {
-        var bag = new Dictionary<string, int>
-        {
-            ["red"] = 12,
-            ["green"] = 13,
-            ["blue"] = 14
-        };
-        
         var answer = 0;
         for (var i = 0; i < input.Length; i++) 
         {
-            var possibleGame = true;
+            var bag = new Dictionary<string, int>();
             foreach (var hand in input[i].Split(": ")[1].Split("; "))
             {
-                var possible = true;
                 foreach (var ball in hand.Split(", "))
                 {
                     var pair = ball.Split(" ");
                     var (count, color) = (int.Parse(pair[0]),pair[1]);
-                    if (bag[color] >= count) continue;
-
-                    possible = false;
-                    break;
-                }
-                if (possible) continue;
-                {
-                    possibleGame = false;
-                    break;
+                    bag.TryAdd(color, 0);
+                    bag[color] = Math.Max(bag[color], count);
                 }
             }
-            if (possibleGame)
-            {
-                answer += i + 1;
-            }
+            answer += bag.Keys.Aggregate(1, (acc, color) => acc * bag[color]);
         }
         Console.WriteLine("Answer: " + answer);
     }

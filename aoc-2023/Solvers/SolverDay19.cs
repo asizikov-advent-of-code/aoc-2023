a5eb18bd5f85a4fcae7e5bb7e0d27c5f33d894e8
Anton Sizikov <asizikov@github.com>
2023-12-19 19:13:31 +0100

    🎁 update formatting ☃️

--- aoc-2023/Solvers/SolverDay19.cs
+++ aoc-2023/Solvers/SolverDay19.cs
@@ -9,10 +9,10 @@ public class SolverDay19 : Solver
         foreach (var line in input)
         {
             if (line == "") break;
-                var key = line[..line.IndexOf('{')];
-                var instructions = line[(line.IndexOf('{') + 1)..^1];
-                var rules = instructions.Split(',');
-                workflows.Add(key, rules);
+            var key = line[..line.IndexOf('{')];
+            var instructions = line[(line.IndexOf('{') + 1)..^1];
+            var rules = instructions.Split(',');
+            workflows.Add(key, rules);
         }
         var answer = 0L;
 
@@ -34,7 +34,7 @@ public class SolverDay19 : Solver
             while (q.Count > 0)
             {
                 var (workflow, parts) = q.Dequeue();
-                
+
                 switch (workflow)
                 {
                     case "R":
@@ -55,43 +55,43 @@ public class SolverDay19 : Solver
                 }
             }
         }
-        
+
         List<(Dictionary<char, (int l, int r)> slice, string next, bool applied)> process(
-            string rule, 
-            Dictionary<char, (int l, int r)> part, 
+            string rule,
+            Dictionary<char, (int l, int r)> part,
             string workflow)
         {
             var left = new Dictionary<char, (int l, int r)>(part);
             var right = new Dictionary<char, (int l, int r)>(part);
-            
-            if (rule.Contains(':'))
+
+            if (!rule.Contains(':')) return [(left, rule, true)];
+
+            var separator = rule.IndexOf(':');
+            var (key, op, dest, value) = (rule[0], rule[1], rule[(separator + 1)..], int.Parse(rule[2..separator]));
+
+            var (l, r) = part[key];
+            switch (op)
             {
-                var separator = rule.IndexOf(':');
-                var (key, op, dest, value) = (rule[0], rule[1], rule[(separator + 1)..], int.Parse(rule[2..separator]));
-                
-                var (l, r) = part[key];
-                switch (op)
-                {
-                    case '<' :
-                        if (l > value) return [(left, workflow, false)];
-                        left[key] = (left[key].l, value - 1);
-                        right[key] = (value, right[key].r);
-                        return
-                        [
-                            (left, dest, true),
-                            (right, workflow, false)
-                        ];
-                    case '>' :
-                        if (r < value) return [(left, workflow, false)];
-                        left[key] = (left[key].l, value);
-                        right[key] = (value + 1, right[key].r);
-                        return                 
-                        [
-                            (left, workflow, false),
-                            (right, dest, true)
-                        ];
-                }
-                        
+                case '<' :
+                    if (l > value) return [(left, workflow, false)];
+
+                    left[key] = (left[key].l, value - 1);
+                    right[key] = (value, right[key].r);
+                    return
+                    [
+                        (left, dest, true),
+                        (right, workflow, false)
+                    ];
+                case '>' :
+                    if (r < value) return [(left, workflow, false)];
+
+                    left[key] = (left[key].l, value);
+                    right[key] = (value + 1, right[key].r);
+                    return
+                    [
+                        (left, workflow, false),
+                        (right, dest, true)
+                    ];
             }
             return [(left, rule, true)];
         }

5af99245c6f717f896e5e4fef3a8b88e2d1a63dd
Anton Sizikov <asizikov@github.com>
2023-12-20 14:04:58 +0100

    🧚‍♀️ trim down number of locs ☃️

--- aoc-2023/Solvers/SolverDay20.cs
+++ aoc-2023/Solvers/SolverDay20.cs
@@ -5,55 +5,38 @@ public class SolverDay20 : Solver
     [PuzzleInput("20-02")]
     public override void Solve(string[] input)
     {
-        var graph = new Dictionary<string, List<string>>();
-        var conjunctions = new Dictionary<string, Dictionary<string ,bool>>();
-        var flipFlops = new Dictionary<string, bool>();
+        var graph = new Dictionary<string, string[]>();
+        var (conjunctions, flipFlops) = (new Dictionary<string, Dictionary<string ,bool>>(),new Dictionary<string, bool>() );
 
         foreach (var line in input)
         {
             var parts = line.Split(" -> ");
-            var machine = parts[0];
-            var connections = parts[1].Split(", ");
+            var (machine, connections) = (parts[0], parts[1].Split(", "));
             var sanitizedName = machine[0] is '%' or '&' ? machine[1..] : machine;
-            
-            switch (machine[0])
-            {
-                case '%':
-                    flipFlops.TryAdd(sanitizedName, false); // off
-                    break;
-                case '&':
-                    conjunctions.Add(sanitizedName, []);
-                    break;
-            }
-            
-            graph.TryAdd(sanitizedName, []);
-            foreach (var connection in connections)
-            {
-                graph[sanitizedName].Add(connection);
-            }
+
+            if (machine[0] is '%') flipFlops.TryAdd(sanitizedName, false); // off
+            else if (machine[0] is '&') conjunctions.Add(sanitizedName, []);
+
+            graph.Add(sanitizedName, connections);
         }
-        
+
         foreach (var key in graph.Keys)
         {
             foreach (var next in graph[key].Where(next => conjunctions.ContainsKey(next)))
             {
-                conjunctions[next].TryAdd(key, true); // low
+                conjunctions[next].Add(key, true); // low
             }
         }
 
         var (informationCollected, buttonPressed) = (false, 0L);
         var memory = conjunctions["nc"]
             .ToDictionary<KeyValuePair<string, bool>, string, long>(inputs => inputs.Key, inputs => 0);
-        
-        while (!informationCollected)
-        {
-            buttonPressed++;
-            sendSignal();
-        }
 
-        Console.WriteLine($": {memory.Values.Aggregate(Lcm)}");
+        while (!informationCollected) sendSignal(++buttonPressed);
 
-        void sendSignal()
+        Console.WriteLine($"Answer: {memory.Values.Aggregate(Lcm)}");
+
+        void sendSignal(long iteration)
         {
             var q = new Queue<(string target, string sender, bool isLow)>();
             q.Enqueue(("broadcaster", "button", true));
@@ -61,23 +44,15 @@ public class SolverDay20 : Solver
             while (q.Count > 0)
             {
                 var (target,sender, isLow) = q.Dequeue();
-                if (target is "broadcaster")
-                {
-                    foreach (var next in graph[target])
-                    {
-                        q.Enqueue((next, target, isLow));
-                    }
-                }
+
+                if (target is "broadcaster") FanOut(target, isLow);
                 else if (flipFlops.TryGetValue(target, out var isOn))
                 {
                     if (!isOn && !isLow) continue;
                     if (!isLow) continue;
 
                     flipFlops[target] = !flipFlops[target];
-                    foreach (var next in graph[target])
-                    {
-                        q.Enqueue((next, target, !flipFlops[target]));
-                    }
+                    FanOut(target, !flipFlops[target]);
                 }
                 else if (conjunctions.TryGetValue(target, out var machine))
                 {
@@ -85,16 +60,14 @@ public class SolverDay20 : Solver
 
                     if (target is "nc" && !isLow)
                     {
-                        if (memory[sender] is 0 ) memory[sender] = buttonPressed;
+                        if (memory[sender] is 0 ) memory[sender] = iteration;
                         if (memory.All(x => x.Value != 0)) informationCollected = true;
                     }
-                    
-                    var signal = machine.Values.All(x => x == false);
-                    foreach (var next in graph[target])
-                    {
-                        q.Enqueue((next, target, signal));
-                    }
+
+                    FanOut(target, machine.Values.All(x => !x));
                 }
+
+                void FanOut(string sender, bool signal) => Array.ForEach(graph[sender],next => q.Enqueue((next, sender, signal)));
             }
         }
 
@@ -102,4 +75,3 @@ public class SolverDay20 : Solver
         long Gcd (long a, long b) => b == 0 ? a : Gcd(b, a % b);
     }
 }
-

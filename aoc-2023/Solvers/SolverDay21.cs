839de8c06a448593e6a2af3e8ed7dcff65c8b305
Anton Sizikov <asizikov@github.com>
2023-12-21 23:10:21 +0100

    🎁 part two 🧚‍♀️

--- aoc-2023/Solvers/SolverDay21.cs
+++ aoc-2023/Solvers/SolverDay21.cs
@@ -1,5 +1,3 @@
-using System.Linq.Expressions;
-
 namespace aoc_2023.Solvers;
 
 public class SolverDay21 : ISolver
@@ -7,8 +5,7 @@ public class SolverDay21 : ISolver
     [PuzzleInput("21-02")]
     public void Solve(string[] input)
     {
-
-        var answer = 0;
+        var answer = 0L;
         for (var r = 0; r < input.Length; r++)
         {
             for (var c = 0; c < input[0].Length; c++)
@@ -18,45 +15,66 @@ public class SolverDay21 : ISolver
             }
         }
         Console.WriteLine("Answer:" + answer);
-
-
+        
         void visit((int r, int c) pos)
         {
+            long queueSizeIncrement = 0;
+            long lastDiff = 0;
+            
             var directions = new (int dr, int dc)[] { (1, 0), (-1, 0), (0, 1), (0, -1) };
             var queue = new Queue<(int r, int c)>();
-            queue.Enqueue(pos);
+            var queueSizeHistory = new List<long> { 0L };
             
-            var steps = 0;
+            queue.Enqueue(pos);
+
+            var steps = 0L;
             while (queue.Count > 0)
             {
                 steps++;
                 var size = queue.Count;
-                
+
                 var unique = new HashSet<(int r, int c)>();
                 while (size --> 0)
                 {
                     var current = queue.Dequeue();
-                    
+
                     foreach (var dir in directions)
                     {
                         (int r, int c) next = (current.r + dir.dr, current.c + dir.dc);
-                        if (next.r < 0 || next.r >= input.Length || next.c < 0 || next.c >= input[0].Length) continue;
-                        if (input[next.r][next.c] == '#') continue;
+                        if (input[(next.r % input.Length + input.Length) % input.Length][(next.c % input[0].Length + input[0].Length) % input[0].Length] == '#') continue;
                         unique.Add(next);
                     }
                 }
-                foreach (var next in unique)
-                {
-                    queue.Enqueue(next);
-                }
+                foreach (var next in unique) queue.Enqueue(next);
 
-                Console.WriteLine("Steps: " + steps + " Size: " + unique.Count);
-                if (steps == 64)
+                if (steps % 100 == 0) 
                 {
-                    answer = unique.Count;
-                    return;
+                    Console.WriteLine(steps + " " + queue.Count);
+                }
+                
+                if (steps % 131 == 65)
+                {
+                    var queueSizeDiff = queue.Count - queueSizeHistory[^1];
+                    var queueSizeGrowth = queueSizeDiff - queueSizeIncrement;
+                    var delta = queueSizeGrowth - lastDiff;
+
+                    queueSizeHistory.Add(queue.Count);
+                    
+                    queueSizeIncrement = queueSizeDiff;
+                    lastDiff = queueSizeGrowth;
+
+                    if (delta is 0) break;
                 }
             }
+            
+            while (steps < 26501365) 
+            {
+                queueSizeIncrement += lastDiff;
+                queueSizeHistory.Add(queueSizeHistory[^1] + queueSizeIncrement);
+                steps += 131;
+            }
+
+            answer = queueSizeHistory[^1];
         }
     }
 }
